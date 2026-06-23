b7a496283443747c54631b11dc7026718bc15b2f
Lakshya07-Goyal <lakshya.goyal.ug24@nsut.ac.in>
2026-06-23 23:55:06 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import json
+
+app=FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
+
+    return data
+
+@app.get("/")
+def hello():
+    return {'message':'Patient Management Record System API'}
+
+@app.get('/about')
+def about():
+    return {'message': 'Functional API to manage patient records'}
+
+@app.get('/display')
+def view():
+    data = load_data()
+    return data
+
